5892ea078d1b1686357eeaa22ac00d4b5594f0bd
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2023-07-15 10:58:47 +0330

    Update Simulator.java

--- src/s2/Simulator.java
+++ src/s2/Simulator.java
@@ -26,7 +26,7 @@ public class Simulator {
 	private int maxqsize;// Max Queue size
 	private int tqs;
 	private Result currentResult;
-	private double lamda;// is the expected (average) number of arrivals per hour,
+	private double lamda; // is the expected (average) number of arrivals per hour,
 							// or day, or whatever units t is measured in.
 	private int id;// event id
 
