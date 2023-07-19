e12b29c0b7a5541fde19b2e0b882c7e4586ad772
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2023-07-19 23:55:54 +0330

    Update Simulator.java

--- src/s2/Simulator.java
+++ src/s2/Simulator.java
@@ -36,7 +36,7 @@ public class Simulator {
 
 	private ServerStatus ss;// server's status
 	/////// metrices////////////
-	double avgDelay = 0; // averageDelay=total delay/npd
+	double avgDelay = 0; // averageDelay is total delay/npd
 	double plr; // packetLossRate;//total packet loss/npa
 	double avgBuffer;// average_Buffer;// total Q size / time it is equalent to Queue size or buffer size
 	private double dt;
