c511f8a219b68022d10f6245cd7c2a15523e3e03
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2023-09-05 23:14:59 +1000

    Update Result.java

--- src/s2/Result.java
+++ src/s2/Result.java
@@ -18,7 +18,7 @@ public class Result implements Cloneable {
 	
 	public String toString() {
 		return String.format("%4.3f\t%s\t%5s\t%5d\t%5d\t%5d\t%5d\t"+
-	"%4.2f\t%4.2f\t%6.2f\t%6.3f\t%6.3f",clock,otype,ss,qLength,
+	"%4.3f\t%4.2f\t%6.2f\t%6.3f\t%6.3f",clock,otype,ss,qLength,
 	npa,npd,ndp,wt,st,lost_ratio,avgDelay,avgQLength);
 	}
 
